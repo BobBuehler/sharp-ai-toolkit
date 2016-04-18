0843c0f6548ea51d99cf2fb6ef9c862e649e04bf
BobB <bob.buehler@gmail.com>
2016-04-18 14:53:52 +0000

    using more

--- BitBoardExtensions.cs
+++ BitBoardExtensions.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
 public static class BitBoardExtensions
 {
     public static bool Get(this BitArray bits, int x, int y)
